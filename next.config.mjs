71ca7c4e6b4a314228da8104f8f31ed8ccc2b160
webamit <amitcse29@gmail.com>
2025-05-25 20:35:39 +0600

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,22 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
     domains: [
       'images.unsplash.com',
       'i.ibb.co',
       'scontent.fotp8-1.fna.fbcdn.net',
     ],
-    // Make ENV
     unoptimized: true,
   },
-}
+  typescript: {
+    ignoreBuildErrors: true, // Ignore TypeScript errors during build
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint errors during build
+  },
+};
 
-export default nextConfig
+export default nextConfig;
